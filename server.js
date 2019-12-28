729305db75591cc8437ca98ed411d0ecdfdbf03c
Nsuliman <nsuliman88@gmail.com>
2019-12-28 14:40:37 +0200

    testing

--- server.js
+++ server.js
@@ -1,22 +1,39 @@
 'use strict';
 
+/***************************************** The Basic *****************************************/
 require('dotenv').config();
 
 const express = require('express');
 
-const pg = require('pg');
+const cors = require('cors');
 
-const server = express();
+const pg = require('pg');
 
 const PORT = process.env.PORT || 3000 ;
 
-const cors = require('cors');
+const server = express();
 
 server.use(cors());
 
+/********************************************* Ruotes  *********************************************/
 
+server.get('/' , (req,res) =>
+{
+    res.status(200).send('The Root/Home Ruote , It\'s Works');
+});
 
+/***************************************** Errors Handlers *****************************************/
 
+// User Error 
+server.use('*' , (req,res) =>
+{
+    res.status(404).send('Not Found');
+});
 
+// Server Error , Any Error
+server.use((error,req,res) =>
+{
+    res.status(500).send(error);
+});
 
 server.listen(PORT , () => console.log(`I am a live , Server Listening on port ${PORT}`));
