36df1b6cf1c97000e59422bd2e8cd82c3ca65351
Nsuliman <nsuliman88@gmail.com>
2019-12-28 15:56:04 +0200

    location Done

--- server.js
+++ server.js
@@ -15,10 +15,43 @@ server.use(cors());
 
 /********************************************* Ruotes  *********************************************/
 
+// test the sever is works , yes it does :) 
 server.get('/' , (req,res) =>
 {
-    res.status(200).send('The Root/Home Ruote , It\'s Works');
+    res.status(200).send('The Root/Home Ruote , It\'s Works ');
 });
+
+/********************************* The Location *************************************/
+// Location 
+
+server.get('/location' , (req,res) =>
+{
+    // res.send('Location Ruote ');
+
+    const locationData = require('./data/geo.json');
+    console.log('locationData : ', locationData);
+    const location = new Location(locationData);
+    console.log('location : ', location);
+    res.status(200).json(location);
+
+}); // end of loaction ruote 
+
+// Location Constructor Function 
+function Location( data ) {
+    this.search_query = 'lynnwood';
+    this.formatted_query = data.results[0].formatted_address;
+    this.latitude = data.results[0].geometry.location.lat;
+    this.longitude = data.results[0].geometry.location.lng;
+  } // end of con.fun of Location 
+
+/********************************* The Weather *************************************/
+// Weather 
+
+server.get('/weather' , (req,res) =>
+{
+    // res.send('Weather Ruote ');
+
+}); // end of weather ruote 
 
 /***************************************** Errors Handlers *****************************************/
 
