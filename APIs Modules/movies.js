fa4d1e2809d50d9239e6cea14d3552604c931b55
Nsuliman <nsuliman88@gmail.com>
2020-01-06 22:32:29 +0200

    Modularization Completed

---
+++ APIs Modules/movies.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const superagent = require('superagent');
+
+module.exports = getMovie;
+
+function getMovie(query) {
+    // console.log('queryyyyyyyyyyyyyyyyyyyyyyy Movies : ', query);
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIES_API_KEY}&query=${query.search_query}`;
+  
+    return superagent.get(url)
+      .then(data => {
+        // console.log('movies Data \n\n : ', data.body);
+        return data.body.results.map(movie => {
+          return new Movies(movie);
+        })
+      })
+      .catch(error => {
+        errorHandler(error,req,res);
+      })
+  
+  }// End of get movies function
+  
+  function Movies(data) {
+    this.title = data.title;
+    this.overview = data.overview;
+    this.average_votes = data.vote_average;
+    this.popularity = data.popularity;
+    this.released_date = data.release_date;
+    this.image_url = `https://image.tmdb.org/t/p/w500${data.poster_path}`;
+  
+  } // End of Movies constructor function 
